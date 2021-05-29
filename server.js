5790f1639e6ee246df7983ffd0e5ef7ee6d16257
bdabak <bayram.dabak@smod.com.tr>
2021-05-29 20:14:51 +0300

    HTTPS

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv").config({ path: __dirname + "/.env" });
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 
 const sendMail = require("./routes/send-mail");
-const { conformsTo } = require("lodash");
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
@@ -18,9 +20,35 @@ if (!port) {
   process.exit(1);
 }
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+if (app.get("env") === "development") {
+  http.createServer(app).listen(process.env.PORT_HTTP, () => {
+    console.log(`Listening on port ${process.env.PORT_HTTP}...`);
+  });
+} else {
+  const options = {
+    key: fs.readFileSync(
+      path.join(
+        __dirname,
+        "../../../etc/letsencrypt/live/www.bayramdabak.com/fullchain.pem"
+      )
+    ),
+    cert: fs.readFileSync(
+      path.join(
+        __dirname,
+        "../../../etc/letsencrypt/live/www.bayramdabak.com/privkey.pem"
+      )
+    ),
+  };
 
-  //Get application environment
-  console.log(`App is running in ${app.get("env")} environment`);
-});
+  https.createServer(options, app).listen(process.env.PORT_HTTPS, () => {
+    console.log(`Listening on port ${process.env.PORT_HTTPS}...`);
+  });
+  // ssl_certificate         /etc/letsencrypt/live/www.bayramdabak.com/fullchain.pem;
+  // ssl_certificate_key     /etc/letsencrypt/live/www.bayramdabak.com/privkey.pem;
+}
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}...`);
+
+//   //Get application environment
+//   console.log(`App is running in ${app.get("env")} environment`);
+// });
